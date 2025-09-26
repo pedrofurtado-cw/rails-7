1b40640ddb0bac2116844ed7eb2dd15174b5387e
Pedro Furtado <pedro.furtado@cloudwalk.io>
2025-09-26 09:14:32 -0300

    feat: changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'render/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'render#index'
+  root to: proc {
+    [
+      200,
+      { "Content-Type" => "application/json" },
+      [ { app: 'funcionou', ok: true, env: Rails.env }.to_json ]
+    ]
+  }
 end
